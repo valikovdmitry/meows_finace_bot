b1369a4d3848ae35781a3ede319e5ec1b816d1a3
valikovdmitry <valikov.dmitry@gmail.com>
2025-01-29 21:59:49 +0300

    Webhook check2

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -4,5 +4,5 @@ from telegram.ext import CallbackContext
 
 async def start(update: Update, context: CallbackContext) -> None:
     await update.message.reply_text(
-        "Веб хук. Это ТОЧНО новая версия! Которая ЗАДЕПЛОИЛАСЬ САМА! ПОЗДРАВЛЯЮЮЮЮЮЮЮ"
+        "Веб хук2. Это ТОЧНО новая версия! Которая ЗАДЕПЛОИЛАСЬ САМА! ПОЗДРАВЛЯЮЮЮЮЮЮЮ"
     )
