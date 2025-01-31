5e822c4bdf376cefb1545ae2f4e454205676d752
valikovdmitry <valikov.dmitry@gmail.com>
2025-01-31 17:33:09 +0300

    Webhook test 4

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -4,5 +4,5 @@ from telegram.ext import CallbackContext
 
 async def start(update: Update, context: CallbackContext) -> None:
     await update.message.reply_text(
-        "Webhook check3. Это ТОЧНО новая версия! Которая ЗАДЕПЛОИЛАСЬ САМА! ПОЗДРАВЛЯЮЮЮЮЮЮЮ"
+        "Webhook test 4. Если все прошло успешно, поздравляю! Вебхук настроен!"
     )
