4bcf0111c9445106aea51c41fa53174341b10ac6
valikovdmitry <valikov.dmitry@gmail.com>
2025-01-31 18:08:34 +0300

    Bug fix hhtp closing 2

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -4,5 +4,5 @@ from telegram.ext import CallbackContext
 
 async def start(update: Update, context: CallbackContext) -> None:
     await update.message.reply_text(
-        "Webhook test 4. Если все прошло успешно, поздравляю! Вебхук настроен!"
+        "http.close test 2."
     )
