3aba94e3da7a766747ec95c153c09a21bcd7e99b
valikovdmitry <valikov.dmitry@gmail.com>
2025-02-04 19:18:35 +0300

    Add docker-compose

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -4,5 +4,5 @@ from telegram.ext import CallbackContext
 
 async def start(update: Update, context: CallbackContext) -> None:
     await update.message.reply_text(
-        "http.close test 2."
+        "Deploy test."
     )
