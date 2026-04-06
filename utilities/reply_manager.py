8f827d23dd1805cf58f1671564b75640115950a1
valikovdmitry <valikov.dmitry@gmail.com>
2026-04-06 19:07:40 +0700

    Update bot footer text to Version 1.0

--- utilities/reply_manager.py
+++ utilities/reply_manager.py
@@ -8,9 +8,9 @@ def format_reply(m_sum, m_cat, m_desc):
     formated_usd = sum_format(usd)
 
     if m_desc:
-        text = f"Так и запишемс! 🐾 \n\n<b>{m_cat[3:]}</b>\n{formated_vnd} VND\n{formated_rub} ₽\n{formated_usd} $\n\nПримечание: {m_desc}\n\nМявс! 🐾\n\nЭто сообщение из новой версии приложения."
+        text = f"Так и запишемс! 🐾 \n\n<b>{m_cat[3:]}</b>\n{formated_vnd} VND\n{formated_rub} ₽\n{formated_usd} $\n\nПримечание: {m_desc}\n\nМявс! 🐾\n\nВерсия 1.0"
     else:
-        text = f"Так и запишемс! 🐾 \n\n<b>{m_cat[3:]}</b>\n{formated_vnd} VND\n{formated_rub} ₽\n{formated_usd} $\n\n\nМявс! 🐾\n\nЭто сообщение из новой версии приложения."
+        text = f"Так и запишемс! 🐾 \n\n<b>{m_cat[3:]}</b>\n{formated_vnd} VND\n{formated_rub} ₽\n{formated_usd} $\n\n\nМявс! 🐾\n\nВерсия 1.0"
 
     return text
 
